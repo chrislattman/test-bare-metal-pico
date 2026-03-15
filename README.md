9dd67151428ea5928833291491520898efb47410
Chris Lattman <natgeo13@gmail.com>
2026-03-15 02:45:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Raspberry Pi Pico 2 W
+
+Instructions:
+
+- Clone https://github.com/raspberrypi/pico-sdk
+- In `~/.bashrc` add `export PICO_SDK_PATH=/path/to/pico-sdk`
+- In the `pico-sdk` folder run `git submodule update --init`
+- Run `sudo apt install build-essential pkg-config libusb-1.0-0-dev cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib`
+
+To build Makefiles: `cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DPICO_BOARD=pico2_w -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/arm-none-eabi-g++ -S . -B build`
+
+To build application: `cmake --build build`
+
+To run on board: 
+
+- Unplug USB cable from board
+- Hold down BOOTSEL button while plugging in USB cable
+- Run `cp build/blink_led.uf2 /media/$USER/RP2350` (flashes the board with the .uf2 file)
---
+++ blink_led.c
@@ -0,0 +1,68 @@
+#include <hardware/flash.h>
+#include <pico/cyw43_arch.h>
+#include <pico/flash.h>
+#include <pico/stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define XIP_OFFSET (256 * 1024)
+
+static void flash_range_erase_callback(void *param)
+{
+    uint32_t offset = (uint32_t)param;
+    flash_range_erase(offset, FLASH_SECTOR_SIZE);
+}
+
+static void flash_range_program_callback(void *param)
+{
+    uint32_t offset = ((uintptr_t*)param)[0];
+    const uint8_t *data = (const uint8_t *)((uintptr_t*)param)[1];
+    flash_range_program(offset, data, FLASH_PAGE_SIZE);
+}
+
+int main()
+{
+    int status;
+    uint8_t write_buffer[FLASH_PAGE_SIZE];
+    uint8_t *read_buffer = (uint8_t *)(XIP_BASE + XIP_OFFSET);
+    uintptr_t params[] = { XIP_OFFSET, (uintptr_t)write_buffer };
+    absolute_time_t next;
+
+    stdio_init_all();
+    status = cyw43_arch_init();
+    if (status != PICO_OK) {
+        printf("Wi-Fi init failed (this is needed for LED on Pico 2 W)");
+        return -1;
+    }
+
+    status = flash_safe_execute(flash_range_erase_callback, (void *)XIP_OFFSET, UINT32_MAX);
+    if (status != PICO_OK) {
+        return -1;
+    }
+    for (size_t i = 0; i < sizeof(write_buffer); i++) {
+        write_buffer[i] = (uint8_t)rand();
+    }
+    status = flash_safe_execute(flash_range_program_callback, params, UINT32_MAX);
+    if (status != PICO_OK) {
+        return -1;
+    }
+    for (size_t i = 0; i < sizeof(write_buffer); i++) {
+        if (read_buffer[i] != write_buffer[i]) {
+            return -1;
+        }
+    }
+
+    next = make_timeout_time_ms(10000);
+    while (1) {
+        if (time_reached(next)) {
+            printf("10 second timer went off\r\n");
+            next = make_timeout_time_ms(10000);
+        }
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(1000);
+        printf("Hello\r\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(1000);
+        printf("World!\r\n");
+    }
+}
